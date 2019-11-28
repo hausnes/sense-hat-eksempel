1d97878a0b2a10612af53bac3853d79aa3531dbf
Jo Bjørnar Hausnes <jb.hausnes@gmail.com>
2019-11-28 20:56:15 +0100

    Create enkel-animasjon-eksempel.py

---
+++ enkel-animasjon-eksempel.py
@@ -0,0 +1,51 @@
+from sense_hat import SenseHat
+import time
+
+s = SenseHat()
+s.low_light = True
+
+green = (0, 255, 0)
+yellow = (255, 255, 0)
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255,255,255)
+nothing = (0,0,0)
+pink = (255,105, 180)
+
+def heart():
+    P = pink
+    O = nothing
+    logo = [
+    O, O, O, O, O, O, O, O,
+    O, P, P, O, P, P, O, O,
+    P, P, P, P, P, P, P, O,
+    P, P, P, P, P, P, P, O,
+    O, P, P, P, P, P, O, O,
+    O, O, P, P, P, O, O, O,
+    O, O, O, P, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+    
+def heart_frame2():
+    P = pink
+    O = nothing
+    logo = [
+    O, P, P, O, P, P, O, O,
+    P, P, P, P, P, P, P, O,
+    P, P, P, P, P, P, P, O,
+    O, P, P, P, P, P, O, O,
+    O, O, P, P, P, O, O, O,
+    O, O, O, P, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    ]
+    return logo
+try:
+    while True: 
+        s.set_pixels(heart())
+        time.sleep(1)
+        s.set_pixels(heart_frame2())
+        time.sleep(0.5)
+ except KeyboardInterrupt:
+    print("Programmet avsluttar.")
